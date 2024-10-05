94ddc1c2e3bc848d139a91c396736f7349c89ca0
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-06 02:38:21 +0300

    Update brain-nok.js

--- games/brain-nok.js
+++ games/brain-nok.js
@@ -4,17 +4,14 @@ const prompt = require("prompt-sync")();
 
 function playNokGame(userName) {
     console.log("You selected 'Find the smallest common multiple of given numbers'.");
-
     for (let round = 0; round < 3; round++) {
         const numbers = [];
-        
         while (numbers.length < 3) {
             const num = Math.floor(Math.random() * 100) + 1;
             if (!numbers.includes(num)) {
                 numbers.push(num);
             }
         }
-        
         console.log(`Round: ${round + 1}\nQuestion: ${numbers.join(", ")}`);
         const userAnswer = parseInt(prompt());
         const correctAnswer = lcmOfArray(numbers);
