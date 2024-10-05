58728faf893273d94fc6d677f8d8a10943fce86a
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-06 02:40:26 +0300

    Update brain-progression.js

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -4,13 +4,10 @@ const prompt = require("prompt-sync")();
 
 function playProgressionGame(userName) {
     console.log("You selected 'What number is missing in the progression'.");
-    
     for (let round = 0; round < 3; round++) {
         const { progression, missingValue } = generateGeometricProgression();
-        
         console.log(`Round: ${round + 1}\n Question: ${progression.join(" ")}`);
         const userAnswer = parseInt(prompt());
-        
         handleAnswer(userAnswer, missingValue, userName);
     }
 }
