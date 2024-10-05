c4e694bc9a1c5da280ea8fe2147146b8972366b5
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-06 01:37:06 +0300

    Update check-answer.js

--- games/check-answer.js
+++ games/check-answer.js
@@ -2,4 +2,13 @@ function checkAnswer(userAnswer, correctAnswer) {
     return userAnswer === correctAnswer;
 }
 
-module.exports = { checkAnswer };
+function handleAnswer(userAnswer, correctAnswer, userName) {
+    if (checkAnswer(userAnswer, correctAnswer)) {
+        console.log("Correct!");
+    } else {
+        console.log(`Your answer: ${userAnswer}.\n It is wrong answer ;(\n Correct answer was ${correctAnswer}. Let's try again, ${userName}!`);
+    }
+
+}
+
+module.exports = { handleAnswer };
