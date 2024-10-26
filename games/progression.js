82642cba45315597c0a541d9113a5323cc8460b2
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-26 13:42:21 +0300

    Update progression.js

--- games/progression.js
+++ games/progression.js
@@ -1,7 +1,7 @@
 const generateGeometricProgression = function() {
     const start = Math.floor(Math.random() * 10) + 1; 
     const ratio = Math.floor(Math.random() * 5) + 1; 
-    const length = 10; // Длина прогрессии
+    const length = 10; 
     const progression = [];
 
     for (let i = 0; i < length; i++) {
