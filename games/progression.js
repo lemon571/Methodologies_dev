dac0b7fe7d36eef9b2733488b6112cfaa39cbe2b
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-26 13:23:37 +0300

    Update progression.js

--- games/progression.js
+++ games/progression.js
@@ -1,14 +1,17 @@
-function generateGeometricProgression() {
-    const start = Math.floor(Math.random() * 10) + 1;
-    const ratio = Math.floor(Math.random() * 5) + 1;
-    const length = 10;
+const generateGeometricProgression = function() {
+    const start = Math.floor(Math.random() * 10) + 1; 
+    const ratio = Math.floor(Math.random() * 5) + 1; 
+    const length = 10; // Длина прогрессии
     const progression = [];
-    const missingIndex = Math.floor(Math.random() * length);
-    const missingValue = progression[missingIndex];
+
     for (let i = 0; i < length; i++) {
         progression.push(start * Math.pow(ratio, i));
     }
+
+    const missingIndex = Math.floor(Math.random() * length);
+    const missingValue = progression[missingIndex];
     progression[missingIndex] = '..'; 
+
     return { progression, missingValue };
 }
 
