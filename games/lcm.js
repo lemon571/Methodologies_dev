cb8a9770a578017eb76c840cf23f2865763b0c7f
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-26 14:00:24 +0300

    Update lcm.js

--- games/lcm.js
+++ games/lcm.js
@@ -1,13 +1,9 @@
-const gcd = function gcd(a, b) {
+const gcd = (a, b) => {
     return b === 0 ? a : gcd(b, a % b);
 };
 
-const lcm = function(a, b) {
-    return (a * b) / gcd(a, b);
-};
+const lcm = (a, b) => (a * b) / gcd(a, b);
 
-const lcmOfArray = function(arr) {
-    return arr.reduce((acc, num) => lcm(acc, num), arr[0]);
-};
+const lcmOfArray = (arr) => arr.reduce((acc, num) => lcm(acc, num), arr[0]);
 
 module.exports = { lcmOfArray };
