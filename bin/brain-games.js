f8ce8d59ed448a64fba84110ba8a14083c2c10d9
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-26 13:40:33 +0300

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,16 +1,16 @@
-const { greetUser } = require("../scr/cli.js");
-const { playNokGame } = require("../games/brain-nok.js");
-const { playProgressionGame } = require("../games/brain-progression.js");
+const { greetUser } = require("../scr/cli.js")
+const { playLcmGame } = require("../games/brain-lcm.js")
+const { playProgressionGame } = require("../games/brain-progression.js")
 const prompt = require("prompt-sync")();
 
-const main = () => {
+const main = function() {
     const userName = greetUser();
     while (true) {
         console.log("Choose a game:\n1. Find the smallest common multiple of given numbers.\n2. What number is missing in the progression?");
         const defense = prompt();
         
         if (defense === '1') {
-            playNokGame(userName);
+            playLcmGame(userName);
         } else if (defense === '2') {
             playProgressionGame(userName);
         } else if (defense === 'exit') {
@@ -22,4 +22,4 @@ const main = () => {
     }
 }
 
-(() => main())();
+main();
