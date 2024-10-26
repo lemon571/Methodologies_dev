8b49d09cdcfc94a152fe5836e3d2c21fba46902c
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-26 15:03:13 +0300

    Update cli.js

--- scr/cli.js
+++ scr/cli.js
@@ -1,11 +1,13 @@
 const prompt = require("prompt-sync")();
 
-function greetUser() {
+const greetUser = () => {
     const WELCOME_TEXT = "Welcome to the Brain Games!\nMay I have your name?";
     console.log(WELCOME_TEXT);
+    
     const name = prompt(); 
     console.log(`Your name: ${name}\nHello, ${name}!`);
+    
     return name;
-}
+};
 
 module.exports = { greetUser };
