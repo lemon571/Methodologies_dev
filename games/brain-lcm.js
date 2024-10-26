bb65ead2ce19f9f1cc02d3a20fcfb59686dbfbd3
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-26 14:54:31 +0300

    Update brain-lcm.js

--- games/brain-lcm.js
+++ games/brain-lcm.js
@@ -4,17 +4,23 @@ const prompt = require("prompt-sync")();
 
 const playLcmGame = (userName) => {
     console.log("You selected 'Find the smallest common multiple of given numbers'.");
+    
     for (let round = 0; round < 3; round++) {
         const numbers = [];
+        
         while (numbers.length < 3) {
             const num = Math.floor(Math.random() * 100) + 1;
+            
             if (!numbers.includes(num)) {
                 numbers.push(num);
             }
         }
+        
         console.log(`Round: ${round + 1}\nQuestion: ${numbers.join(", ")}`);
+        
         const userAnswer = parseInt(prompt());
         const correctAnswer = lcmOfArray(numbers);
+        
         handleAnswer(userAnswer, correctAnswer, userName);
     }
 };
