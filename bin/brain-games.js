f689b190cdad11aa09b23a41b1dc06dd38e2b3c5
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-26 15:02:13 +0300

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,22 +1,29 @@
+const prompt = require("prompt-sync")();
 const { greetUser } = require("../scr/cli.js")
 const { playLcmGame } = require("../games/brain-lcm.js")
 const { playProgressionGame } = require("../games/brain-progression.js")
-const prompt = require("prompt-sync")();
 
 const main = () => {
     const userName = greetUser();
+    
     while (true) {
         console.log("Choose a game:\n1. Find the smallest common multiple of given numbers.\n2. What number is missing in the progression?");
+       
         const defense = prompt();
-        if (defense === '1') {
-            playLcmGame(userName);
-        } else if (defense === '2') {
-            playProgressionGame(userName);
-        } else if (defense === 'exit') {
-            console.log(`Goodbye, ${userName}!`);
-            process.exit();
-        } else {
-            console.log("Invalid choice. Please select again.");
+        
+        switch (defense) {
+            case '1':
+                playLcmGame(userName);
+                break;
+            case '2':
+                playProgressionGame(userName);
+                break;
+            case 'exit':
+                console.log("Goodbye, ${userName}!");
+                process.exit();
+                break;
+            default:
+                console.log("Invalid choice. Please select again.");
         }
     }
 };
