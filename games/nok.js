4481f9499701c78e51172d4b6b3b222b95fbe060
lemon571 <91371812+lemon571@users.noreply.github.com>
2024-10-26 13:23:54 +0300

    Update nok.js

--- games/nok.js
+++ games/nok.js
@@ -1,13 +1,13 @@
-function gcd(a, b) {
+const gcd = function gcd(a, b) {
     return b === 0 ? a : gcd(b, a % b);
-}
+};
 
-function lcm(a, b) {
+const lcm = function(a, b) {
     return (a * b) / gcd(a, b);
-}
+};
 
-function lcmOfArray(arr) {
+const lcmOfArray = function(arr) {
     return arr.reduce((acc, num) => lcm(acc, num), arr[0]);
-}
+};
 
 module.exports = { lcmOfArray };
